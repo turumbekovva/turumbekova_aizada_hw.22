dd1e956f826437259b8f4627b57b075872a5b321
turumbekovva <turunbekovaaizada@gmail.com>
2023-09-06 16:48:23 +0600

    hw

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        int personsAge = 14;
-        int temperature = 22;
+        System.out.println(ageAndTemperature(14,30));
+        System.out.println(ageAndTemperature(16,25));
+        System.out.println(ageAndTemperature(17,20));
+        System.out.println(ageAndTemperature(10,23));
+        System.out.println(ageAndTemperature(12,29));
+    }
+    public static String ageAndTemperature(int personsAge,int temperature){
 
-        if( personsAge <= 20 && personsAge >= 45   && temperature <=-20 && temperature >=30) {
-            System.out.println("Можно идти гулять");
+        if( personsAge > 20 && personsAge < 45 && temperature >-20 && temperature <30) {
+            return "Можно идти гулять";
         }
-        else if (personsAge < 20 && temperature <= 0 && temperature>= 28) {
-            System.out.println("Можно идти гулять");
+        else if (personsAge < 20 && temperature > 0 && temperature< 28) {
+            return "Можно идти гулять";
         }
-        else if ( personsAge > 45 && temperature <-10 && temperature>25) {
-            System.out.println("Можно идти гулять");
+        else if ( personsAge > 45 && temperature >-10 && temperature<25) {
+            return "Можно идти гулять";
         }
         else {
-            System.out.println("Оставайтесь дома");
+            return "оставайтесь дома";
         }
-
     }
+
 }
